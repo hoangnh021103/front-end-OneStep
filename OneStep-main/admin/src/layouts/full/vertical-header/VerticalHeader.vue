f93570ae016d3bb6935ba50b628725cb9801dcf7
thienldph52917 <162966519+thienldph52917@users.noreply.github.com>
2025-09-06 08:50:34 +0700

    Add files via upload

--- OneStep-main/admin/src/layouts/full/vertical-header/VerticalHeader.vue
+++ OneStep-main/admin/src/layouts/full/vertical-header/VerticalHeader.vue
@@ -1,8 +1,7 @@
 <script setup lang="ts">
-import { ref } from 'vue';
 import { useCustomizerStore } from '../../../stores/customizer';
 // Icon Imports
-import { BellIcon, SettingsIcon, SearchIcon, Menu2Icon } from 'vue-tabler-icons';
+import { BellIcon, SettingsIcon, Menu2Icon } from 'vue-tabler-icons';
 
 // dropdown imports
 import NotificationDD from './NotificationDD.vue';
@@ -10,10 +9,6 @@ import ProfileDD from './ProfileDD.vue';
 import Searchbar from './SearchBarPanel.vue';
 
 const customizer = useCustomizerStore();
-const showSearch = ref(false);
-function searchbox() {
-  showSearch.value = !showSearch.value;
-}
 </script>
 
 <template>
@@ -41,31 +36,8 @@ function searchbox() {
       <Menu2Icon size="20" stroke-width="1.5" />
     </v-btn>
 
-    <!-- search mobile -->
-    <v-btn
-      class="hidden-lg-and-up text-secondary ml-3"
-      color="lightsecondary"
-      icon
-      rounded="sm"
-      variant="flat"
-      size="small"
-      @click="searchbox"
-    >
-      <SearchIcon size="17" stroke-width="1.5" />
-    </v-btn>
 
-    <v-sheet v-if="showSearch" class="search-sheet v-col-12">
-      <Searchbar :closesearch="searchbox" />
-    </v-sheet>
 
-    <!-- ---------------------------------------------- -->
-    <!-- Search part -->
-    <!-- ---------------------------------------------- -->
-    <v-sheet class="mx-3 v-col-3 v-col-xl-2 v-col-lg-4 d-none d-lg-block">
-      <Searchbar />
-    </v-sheet>
-
-    <!---/Search part -->
 
     <v-spacer />
     <!-- ---------------------------------------------- -->
