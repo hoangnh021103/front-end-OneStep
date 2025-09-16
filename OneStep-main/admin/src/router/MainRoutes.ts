9950e41afd67b149d892bfb4d8711fb26db96b28
hoangnh021103 <huyhoangdz02112003@gmail.com>
2025-09-16 21:26:18 +0700

    .

--- OneStep-main/admin/src/router/MainRoutes.ts
+++ OneStep-main/admin/src/router/MainRoutes.ts
@@ -17,6 +17,11 @@ const MainRoutes = {
       component: () => import('@/views/bang-dieu-khien/default/DefaultDashboard.vue'),
     },
     {
+      name: 'BanHang',
+      path: '/quan-ly/ban-hang',
+      component: () => import('@/views/quan-ly/ban-hang/BanHang.vue'),
+    },
+    {
       name: 'QuanLyHoaDon',
       path: '/quan-ly/hoa-don',
       component: () => import('@/views/quan-ly/hoa-don/DanhSachHoaDon.vue'),
