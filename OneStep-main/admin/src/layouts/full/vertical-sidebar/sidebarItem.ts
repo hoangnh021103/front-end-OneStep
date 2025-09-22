8291b79a5fe0f9acc55f1d2ad00b52799c0bf12e
hoangnh021103 <huyhoangdz02112003@gmail.com>
2025-09-22 12:50:01 +0700

    .

--- OneStep-main/admin/src/layouts/full/vertical-sidebar/sidebarItem.ts
+++ OneStep-main/admin/src/layouts/full/vertical-sidebar/sidebarItem.ts
@@ -103,10 +103,6 @@ const sidebarItem: menu[] = [
     icon: CircleIcon,
     to: '/quan-ly/phieu-giam-gia'
   },
-  { title: ' thanh toán',
-    icon: CircleIcon,
-    to: '/quan-ly/thanh-toan'
-  },
 
   // {
   //   title: 'Thư viện ảnh',
